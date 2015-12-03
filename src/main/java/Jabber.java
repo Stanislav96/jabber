17ac7a8a30e65ff585233de18fc08c1a02f921b1
Stanislav96 <baklanov.stas@ya.ru>
2015-12-03 19:40:41 +0300

    Program 0.1
    program can start connecting with server

--- src/main/java/Jabber.java
+++ src/main/java/Jabber.java
@@ -1,61 +1,328 @@
-import org.jivesoftware.smack.*;
-import org.jivesoftware.smack.packet.Message;
-import org.jivesoftware.smack.packet.Presence;
-import org.jivesoftware.smack.packet.Stanza;
-import org.jivesoftware.smack.tcp.XMPPTCPConnection;
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.helpers.XMLReaderFactory;
 
-import java.util.Collection;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.Charset;
+import java.util.Base64;
+import java.util.StringJoiner;
 
 public class Jabber {
-  private AbstractXMPPConnection conn;
-  private Chat chat = null;
+  private Socket socket;
+  private BufferedWriter writer;
+  private InputStream in;
+  private String user;
+  private String pass;
+  private String receiver = "";
+  volatile private boolean logged = false;
+  private boolean connected = false;
+  private boolean closed = false;
 
-  private class Listener implements MessageListener {
-    public void processMessage(Chat chat, Message message) {
-      System.out.println("<< " + message);
+  private class ListenerThread extends Thread {
+    private InputStream inForParser;
+    private InputStream in;
+    private boolean message = false;
+    private boolean body = false;
+    private boolean presence = false;
+    private boolean show = false;
+    private boolean mechanisms = false;
+    private boolean mechanism = false;
+    private boolean hasMechanisms = false;
+    private boolean hasPlain = false;
+    private boolean features = false;
+    private String sender;
+    private SAXParser parser;
+
+    private class Listener extends DefaultHandler {
+      @Override
+      public void skippedEntity(String name) throws SAXException {}
+
+      @Override
+      public void startElement(String namespaceURI, String localName, String qName, Attributes atts) throws
+          SAXException {
+        System.out.println("begin Elem: "+qName);
+        if (qName.equals("message")) {
+          message = true;
+          sender = atts.getValue("from");
+          if (sender == null) {
+            throw new SAXException("no from");
+          }
+          return;
+        }
+        if (qName.equals("body") && message) {
+          body = true;
+          return;
+        }
+        if (qName.equals("presence")) {
+          presence = true;
+          sender = atts.getValue("from");
+          if (sender == null) {
+            throw new SAXException("no from");
+          }
+          return;
+        }
+        if (qName.equals("show") && presence) {
+          body = true;
+          return;
+        }
+        if (qName.equals("stream:features")) {
+          features = true;
+          return;
+        }
+        if (qName.equals("starttls") && features) {
+          try {
+            writer.write("<starttls xmlns=\"urn:ietf:params:xml:ns:xmpp-tls\"/>");
+            writer.flush();
+          } catch (IOException e) {
+            e.printStackTrace();
+            close();
+          }
+          return;
+        }
+        if (qName.equals("proceed")) {
+          try {
+            parser.reset();
+            writer.write("<?xml version=\"1.0\"?>" +
+                "<stream:stream xmlns:stream=\"http://etherx.jabber.org/streams\" " +
+                "version=\"1.0\" "+
+                "xmlns=\"jabber:client\" " +
+                "to=\"jabber.zone\" " +
+                "xml:lang=\"ru\" " +
+                "xmlns:xml=\"http://www.w3.org/XML/1998/namespace\">");
+            writer.flush();
+          } catch (IOException e) {
+            e.printStackTrace();
+            close();
+          }
+          return;
+        }
+        if (qName.equals("mechanisms") && features) {
+          hasMechanisms = true;
+          mechanisms = true;
+          return;
+        }
+        if (qName.equals("mechanism") && mechanisms) {
+          mechanism = true;
+          return;
+        }
+        if (qName.equals("success")) {
+          logged = true;
+        }
+      }
+
+      @Override
+      public void endElement(String namespaceURI, String localName, String qName) throws SAXException {
+        System.out.println("end Elem: "+ qName);
+        if (qName.equals("message")) {
+          message = false;
+          return;
+        }
+        if (qName.equals("body") && message) {
+          body = false;
+          return;
+        }
+        if (qName.equals("presence")) {
+          presence = false;
+          return;
+        }
+        if (qName.equals("show") && presence) {
+          show = false;
+          return;
+        }
+        if (qName.equals("stream:features")) {
+          features = false;
+          if (!hasMechanisms) {
+            logged = true;
+          }
+          return;
+        }
+        if (qName.equals("mechanisms") && features) {
+          if (!hasPlain) {
+            throw new SAXException("no plain auth");
+          }
+          mechanisms = false;
+          return;
+        }
+        if (qName.equals("mechanism") && mechanisms) {
+          mechanism = false;
+          return;
+        }
+        if (qName.equals("success")) {
+          try {
+            parser.reset();
+            writer.write("<?xml version=\"1.0\"?>" +
+                "<stream:stream xmlns:stream=\"http://etherx.jabber.org/streams\" " +
+                "version=\"1.0\" "+
+                "xmlns=\"jabber:client\" " +
+                "to=\"jabber.zone\" " +
+                "xml:lang=\"ru\" " +
+                "xmlns:xml=\"http://www.w3.org/XML/1998/namespace\">");
+            writer.flush();
+          } catch (IOException e) {
+            e.printStackTrace();
+            close();
+            return;
+          }
+          logged = true;
+        }
+      }
+
+      @Override
+      public void characters(char[] ch, int start, int length) throws SAXException {
+        System.out.println("characters: "+new String(ch, start, length));
+        if (body) {
+          System.out.println("<< new msg from" + sender + "\n" + "<< " + new String(ch, start, length));
+          return;
+        }
+        if (show) {
+          System.out.println("<< " + sender + " is now " + new String(ch, start, length));
+          return;
+        }
+        if (mechanism) {
+          if (new String(ch, start, length).equals("PLAIN")) {
+            hasPlain = true;
+            Charset charset = Charset.forName("US-ASCII");
+            String code = Base64.getEncoder()
+                .encodeToString(charset.encode(user.split("@")[0]).put((byte) 0).put(charset.encode(pass)).array());
+            try {
+              writer.write("<auth " +
+                  "xmlns=\"urn:ietf:params:xml:ns:xmpp-sasl\" " +
+                  "mechanism=\"PLAIN\">" +
+                  code +
+                  "</auth>");
+              writer.flush();
+            } catch (IOException e) {
+              e.printStackTrace();
+              close();
+            }
+          }
+        }
+      }
+    }
+
+    ListenerThread(InputStream in) {
+      this.in = in;
+    }
+
+    public void run() {
+      SAXParserFactory spf = SAXParserFactory.newInstance();
+      //XMLReader reader;
+      try {
+        parser = spf.newSAXParser();
+        //reader = XMLReaderFactory.createXMLReader();
+        //reader.setContentHandler(new Listener());
+      } catch (SAXException | ParserConfigurationException e) {
+        e.printStackTrace();
+        close();
+        return;
+      }
+      try {
+        //reader.parse(new InputSource(in));
+        parser.parse(in, new Listener());
+      } catch (SAXException | IOException e) {
+        e.printStackTrace();
+        close();
+      }
     }
   }
 
   public Jabber() {
-    ChatManager.getInstanceFor(conn).addChatListener((chat, createdLocally) -> {
-      if (!createdLocally) {
-        chat.addMessageListener(new Listener());
-      }
-    });
-    //Roster.getInstanceOf().
-    conn.getRoster().addRosterListener(new RosterListener() {
-      public void entriesAdded(Collection<String> addresses) {}
-      public void entriesDeleted(Collection<String> addresses) {}
-      public void entriesUpdated(Collection<String> addresses) {}
-      public void presenceChanged(Presence presence) {
-        System.out.println("<< " + presence.getFrom() + " " + presence);
-      }
-    });
+    try {
+      socket = new Socket("jabber.zone", 5222);
+      in = socket.getInputStream();
+      writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+    } catch (IOException e) {
+      e.printStackTrace();
+      close();
+      return;
+    }
+    connected = true;
   }
 
-  public void login(String user, String pass, String serv) throws Exception {
-    conn = new XMPPTCPConnection(user, pass, serv);
-    conn.connect().login();
+  public boolean isConnected() {
+    return connected;
   }
 
-  public void chat(String user) {
-    chat = ChatManager.getInstanceFor(conn).createChat(user, new Listener());
+  public void login(String user, String pass) throws Exception {
+    this.user = user;
+    this.pass = pass;
+    writer.write("<?xml version=\"1.0\"?>" +
+        "<stream:stream xmlns:stream=\"http://etherx.jabber.org/streams\" " +
+        "version=\"1.0\" "+
+        "xmlns=\"jabber:client\" " +
+        "to=\"jabber.zone\" " +
+        "xml:lang=\"ru\" " +
+        "xmlns:xml=\"http://www.w3.org/XML/1998/namespace\">");
+    writer.flush();
+    Thread thr = new ListenerThread(in);
+    thr.setDaemon(true);
+    thr.start();
+  }
+
+  public void receiver(String user) throws Exception {
+    receiver = user;
   }
 
   public void sendMsg(String msg) throws Exception {
-    if (chat != null) {
-      chat.sendMessage(msg);
+    if (!logged) {
+      throw new Exception("not logged in");
     }
+    if (receiver.isEmpty()) {
+      throw new Exception("no receiver");
+    }
+    writer.write("<message" +
+        "from = >" + user +
+        " to = " + receiver +
+        "<body>" +
+        msg +
+        "</body>" +
+        "</message>");
+    writer.flush();
   }
 
   public void sendStatus(String status) throws Exception {
-    if (status.equals("here")) {
-      //conn.sendStanza(new Presence(Presence.Type.available));
-      conn.sendPacket(new Presence(Presence.Type.available));
+    if (!logged) {
+      throw new Exception("not logged in");
     }
-    if (status.equals("away")) {
-      //conn.sendStanza(new Presence(Presence.Type.unavailable));
-      conn.sendPacket(new Presence(Presence.Type.unavailable));
+    writer.write("<presence>" +
+        "<show>" +
+        status +
+        "</show>" +
+        "</presence>");
+    writer.flush();
+  }
+
+  public synchronized void close() {
+    if (closed) {
+      return;
     }
+    closed = true;
+   try {
+     if (connected) {
+       connected = false;
+       writer.write("</stream>");
+       writer.flush();
+     }
+   } catch (IOException e) {
+     e.printStackTrace();
+   } finally {
+     try {
+       if (socket != null) {
+         socket.close();
+       }
+     } catch (IOException e) {
+       e.printStackTrace();
+     }
+   }
   }
 }
+/*
+input baklanov.stas@jabber.zone 123ghj
+ */
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,43 +1,59 @@
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
     Jabber jabber = new Jabber();
-    while (true) {
-      String req = System.console().readLine(">>");
+    while (jabber.isConnected()) {
+      Scanner scanner = new Scanner(System.in);
+      System.out.print("<< ");
+      String req = scanner.next();
       if (req.equals("input")) {
-        String serv = System.console().readLine();
-        String user = System.console().readLine();
-        String pass = new String(System.console().readPassword());
+        String user = scanner.next();
+        String pass = scanner.next();
         try {
-          jabber.login(user, pass, serv);
+          jabber.login(user, pass);
         } catch (Exception e) {
+          jabber.close();
           e.printStackTrace();
           return;
         }
         continue;
       }
-      if (req.equals("chat")) {
-        String user = System.console().readLine();
-        jabber.chat(user);
+      if (req.equals("to")) {
+        String user = scanner.next();
+        try {
+          jabber.receiver(user);
+        } catch (Exception e) {
+          jabber.close();
+          e.printStackTrace();
+          return;
+        }
+        continue;
       }
       if (req.equals("msg")) {
-        String msg = System.console().readLine();
+        String msg = scanner.next();
         try {
           jabber.sendMsg(msg);
         } catch (Exception e) {
+          jabber.close();
           e.printStackTrace();
           return;
         }
+        continue;
       }
       if (req.equals("status")) {
-        String status = System.console().readLine();
+        String status = scanner.next();
         try {
           jabber.sendStatus(status);
         } catch (Exception e) {
+          jabber.close();
           e.printStackTrace();
           return;
         }
+        continue;
       }
       if (req.equals("exit")) {
+        jabber.close();
         return;
       }
     }
